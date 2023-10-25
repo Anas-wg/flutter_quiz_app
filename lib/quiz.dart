5c18946df39e6ae0f54562fd10696baaebcb9524
CHO WangGi <70210457+Anas-wg@users.noreply.github.com>
2023-10-25 15:02:35 +0900

    Code by lecture

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,28 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/data/questions.dart';
-import 'package:quiz_app/question_screen.dart';
+
 import 'package:quiz_app/start_quiz.dart';
+import 'package:quiz_app/question_screen.dart';
+import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/result_screen.dart';
 
 class Quiz extends StatefulWidget {
-  // 퀴즈 상태 전체를 관리
   const Quiz({super.key});
 
   @override
   State<Quiz> createState() {
-    return _Quizstate();
+    return _QuizState();
   }
 }
 
-class _Quizstate extends State<Quiz> {
-  // 선택한 답들을 담는 그릇
+class _QuizState extends State<Quiz> {
   List<String> selectedAnswers = [];
-  // initSTate 시행전 null 값
   var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = 'question-screen';
+      activeScreen = 'questions-screen';
     });
   }
 
@@ -36,19 +34,27 @@ class _Quizstate extends State<Quiz> {
     }
   }
 
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'questions-screen';
+    });
+  }
+
   @override
   Widget build(context) {
     Widget screenWidget = StartQuiz(switchScreen);
 
-    if (activeScreen == 'question-screen') {
+    if (activeScreen == 'questions-screen') {
       screenWidget = QuestionScreen(
         onSelectAnswer: chooseAnswer,
       );
     }
 
     if (activeScreen == 'results-screen') {
-      screenWidget = ResultScreen(
+      screenWidget = ResultsScreen(
         chosenAnswers: selectedAnswers,
+        onRestart: restartQuiz,
       );
     }
 
@@ -56,10 +62,14 @@ class _Quizstate extends State<Quiz> {
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 91, 21, 213),
-              Color.fromARGB(255, 69, 16, 160),
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
           child: screenWidget,
         ),
--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+
 import 'package:quiz_app/data/questions.dart';
-import 'package:quiz_app/question_summary.dart';
+import 'package:quiz_app/questions_summary/question_summary.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class ResultScreen extends StatelessWidget {
-  const ResultScreen({
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({
     super.key,
     required this.chosenAnswers,
+    required this.onRestart,
   });
 
+  final void Function() onRestart;
   final List<String> chosenAnswers;
 
   List<Map<String, Object>> getSummaryData() {
@@ -19,7 +22,6 @@ class ResultScreen extends StatelessWidget {
         {
           'question_index': i,
           'question': questions[i].text,
-          // 정답은 첫번째(idx: 0)
           'correct_answer': questions[i].answers[0],
           'user_answer': chosenAnswers[i]
         },
@@ -30,7 +32,7 @@ class ResultScreen extends StatelessWidget {
   }
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     final summaryData = getSummaryData();
     final numTotalQuestions = questions.length;
     final numCorrectQuestions = summaryData.where((data) {
@@ -45,25 +47,28 @@ class ResultScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'Your answered $numCorrectQuestions out of $numTotalQuestions question correctly',
+              'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!',
               style: GoogleFonts.lato(
-                  color: const Color.fromARGB(255, 153, 113, 224),
-                  fontSize: 16),
+                color: const Color.fromARGB(255, 230, 200, 253),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
             ),
             const SizedBox(
               height: 30,
             ),
-            QuestionSummary(summaryData),
+            QuestionsSummary(summaryData),
             const SizedBox(
               height: 30,
             ),
-            OutlinedButton.icon(
-              onPressed: () {},
-              style: OutlinedButton.styleFrom(
+            TextButton.icon(
+              onPressed: onRestart,
+              style: TextButton.styleFrom(
                 foregroundColor: Colors.white,
               ),
-              icon: const Icon(Icons.arrow_circle_right),
-              label: const Text('Start Quiz'),
+              icon: const Icon(Icons.refresh),
+              label: const Text('Restart Quiz!'),
             )
           ],
         ),
